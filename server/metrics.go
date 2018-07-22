6e410b237f99df8a1434e30982b97b3c136ec370
Curtis Mattoon <cmattoon@entic.com>
2018-07-22 18:32:59 -0400

    Make metricz a little more useful

--- server/metrics.go
+++ server/metrics.go
@@ -1,14 +1,36 @@
 package server
 
 import (
+	"fmt"
 	"encoding/json"
 	"net/http"
+	"runtime"
 )
 
-type Metric struct {
-	Name string
-	Time string
-	Values map[string]string
+type Metrics struct {
+	Store map[string]string
+	Memory map[string]string
+}
+
+
+func (s *httpServer) collectMetrics() (*Metrics) {
+
+	var mem runtime.MemStats
+	runtime.ReadMemStats(&mem)
+
+	mm := make(map[string]string)
+	mm["MemAlloc"] = fmt.Sprintf("%v", mem.Alloc)
+	mm["MemTotalAlloc"] = fmt.Sprintf("%v", mem.TotalAlloc)
+	mm["MemSys"] = fmt.Sprintf("%v", mem.Sys)
+	mm["MemNumGC"] = fmt.Sprintf("%v", mem.NumGC)
+
+	ss := make(map[string]string)
+	ss["Size"] = fmt.Sprintf("%v", len(s.storeMap))
+
+	return &Metrics{
+		Store: ss,
+		Memory: mm,
+	}
 }
 
 func (s *httpServer) healthCheck(w http.ResponseWriter, _ *http.Request) {
@@ -18,11 +40,14 @@ func (s *httpServer) healthCheck(w http.ResponseWriter, _ *http.Request) {
 
 func (s *httpServer) doMetrics(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	m := Metric{"Foobar", "11:39", map[string]string{"key": "value"}}
-	js, err := json.Marshal(m)
+
+	m := s.collectMetrics()
+	js, err := json.Marshal(*m)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
 }
